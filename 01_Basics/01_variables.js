cb45433a5281222d1df06b900332cbe14318943e
RamanBajaj <97776914+RamanBajaj@users.noreply.github.com>
2025-02-02 06:58:48 +0000

    datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553;
+let accountEmail = "raman@1234";
+var accountPassword = "12345";
+accountCity = "Delhi";
+let accountState;
+
+//accountId = 23456;
+
+accountEmail = "ramanbajaj@1222";
+accountPassword = "23456";
+accountCity = "Jaipur";
+
+console.log(accountId);
+console.table([accountEmail, accountPassword, accountCity, accountState]);
